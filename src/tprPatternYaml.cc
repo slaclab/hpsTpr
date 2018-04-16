7e6bd30fc2d20f3503609ec4be59c3ef4c502274
Kukhee Kim <khkim@lcls-dev3.slac.stanford.edu>
2018-04-16 15:27:19 -0700

    Remove default crossbar control, The crossbarControl module will set up crossbar switch properly, or, just keep use current crossbar control setting until user's intervention

--- src/tprPatternYaml.cc
+++ src/tprPatternYaml.cc
@@ -25,7 +25,7 @@ TprPatternYaml::TprPatternYaml(Path core, Path tstream)
     _read_cnt = 0;
     
     // SetupTimingFrameInput(0); /* use TPGMini timing frame from FPGA */
-    SetupTimingFrameInput(1); /* use timing from backplane */
+    // SetupTimingFrameInput(1); /* use timing from backplane */
     
     // SetupMsgNoDelay(0);    /* delay 3 fiducials for LCLS1 timing pattern delivery */
     SetupMsgNoDelay(1);    /* prompt delivery for LCLS1 timing pattern */
